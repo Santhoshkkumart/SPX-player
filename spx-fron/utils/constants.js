2f347ad1bf9819ecd1fe8ae1fd6ae44fbe400fe4
santhoshkkumart <santhoshkkumarsan@gmail.com>
2026-09-19 16:56:05 +0530

    fixed ports and readme

--- spx-fron/utils/constants.js
+++ spx-fron/utils/constants.js
@@ -1,6 +1,6 @@
-// Use the specific LAN IP for all environments
-export const ANDROID_EMULATOR_URL = 'http://192.168.31.84:3000';
-export const DEFAULT_BACKEND_URL = 'http://192.168.31.84:3000';
+// Default backend URL - replace with your actual server IP or use EXPO_PUBLIC_BACKEND_URL env variable
+export const ANDROID_EMULATOR_URL = 'http://localhost:3000';
+export const DEFAULT_BACKEND_URL = 'http://localhost:3000';
 
 export const COVER_IMAGES = [
   'https://images.unsplash.com/photo-1614613535308-eb5fbd3d2c17?q=80&w=1000&auto=format&fit=crop',
